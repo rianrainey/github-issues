aa8e330caa89b35cd4b2f0d6aba4ad25c4b54930
Rian Rainey <hello@rian.me>
2015-12-26 17:56:36 -0500

    Add documentation and tests

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir hello@rian.me"} ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -17,9 +21,14 @@ defmodule Issues.GithubIssues do
   end
   def handle_response({:ok, %HTTPoison.Response{status_code: status,
                                                 body: body}}) when status >= 400 do
+    Logger.error "Error #{status} returned"
+
     {:error, body}
   end
   def handle_response({:ok, %HTTPoison.Response{body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
+
     {:ok, :jsx.decode(body)}
   end
 end
